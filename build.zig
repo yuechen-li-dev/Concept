4307aa3432a026546fc5ddec0677f7d36a824b45
Yuechen Li <109704248+yuechen-li-dev@users.noreply.github.com>
2026-06-05 19:03:19 -0700

    Update reporting instructions

--- build.zig
+++ build.zig
@@ -4,16 +4,16 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const parser_tests = b.addTest(.{
+    const compiler_tests = b.addTest(.{
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/compiler/parser.zig"),
+            .root_source_file = b.path("src/compiler/tests.zig"),
             .target = target,
             .optimize = optimize,
         }),
     });
 
-    const run_parser_tests = b.addRunArtifact(parser_tests);
+    const run_compiler_tests = b.addRunArtifact(compiler_tests);
 
     const test_step = b.step("test", "Run compiler tests");
-    test_step.dependOn(&run_parser_tests.step);
+    test_step.dependOn(&run_compiler_tests.step);
 }
