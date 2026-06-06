966cbf459e2a8011f4cb8ba957579709a5888db2
Yuechen Li <109704248+yuechen-li-dev@users.noreply.github.com>
2026-06-05 19:15:18 -0700

    Add diagnostics core

--- src/compiler/tests.zig
+++ src/compiler/tests.zig
@@ -1,3 +1,4 @@
+pub const diagnostics = @import("diagnostics.zig");
 pub const parser = @import("parser.zig");
 pub const source = @import("source.zig");
 pub const testing = @import("testing/harness.zig");
